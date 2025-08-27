01efa4bfe138ed125d9163b79e79e38071cac459
PetraSnajdrova <petrasnajdrova@seznam.cz>
2025-08-27 19:56:47 +0200

    Task_2.sql

--- Task_2.sql
+++ Task_2.sql
@@ -4,7 +4,7 @@
  */
 WITH cte_milk_bread_purchase AS (
 	SELECT *,
-		round((avg_salary_value / avg_goods_value)::NUMERIC,1) AS How_many_units
+		round((avg_salary_value / avg_goods_value)::NUMERIC,1) AS how_many_units
 	FROM t_petra_snajdrova_project_sql_primary_final tpspspf
 	WHERE goods_name LIKE 'Mléko polotučné pasterované'
 		OR goods_name LIKE 'Chléb konzumní kmínový'
@@ -12,9 +12,10 @@ WITH cte_milk_bread_purchase AS (
 SELECT
 	year,
 	goods_name,
-	avg(How_many_units) AS Possible_purchase,
+	avg(How_many_units) AS possible_purchase,
 	price_unit
 FROM cte_milk_bread_purchase
 WHERE year = 2006 OR year = 2018
 GROUP BY year, goods_name, price_unit
 ORDER BY year ASC, goods_name asc;
+
