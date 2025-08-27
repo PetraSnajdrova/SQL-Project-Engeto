613ac8ec96baf2922966f3c8fd47ec6d556f932a
PetraSnajdrova <petrasnajdrova@seznam.cz>
2025-08-27 19:52:23 +0200

    Task_1.sql
    Upraveny názvy sloupců dle pravidel snake_case

--- Task_1.sql
+++ Task_1.sql
@@ -4,21 +4,22 @@ WITH cte_avg_salary_previous_year AS (
 	DISTINCT branch_name,
 	year,
 	avg_salary_value,
-	lag(Avg_salary_value) OVER (PARTITION BY Branch_name ORDER BY year) AS Avg_salary_previous_year
+	lag(avg_salary_value) OVER (PARTITION BY branch_name ORDER BY year) AS avg_salary_previous_year
 	FROM t_petra_snajdrova_project_SQL_primary_final
 	WHERE branch_name IS NOT NULL
 )
 SELECT
 	year,
-	Branch_name,
+	branch_name,
 	avg_salary_value,
-	Avg_salary_previous_year,
+	avg_salary_previous_year,
 	CASE
 		WHEN avg_salary_previous_year IS NULL THEN 'N/A'
 		WHEN avg_salary_value - avg_salary_previous_year = 0 THEN 'duplicated data'
-		WHEN avg_salary_value - avg_salary_previous_year > 0 THEN 'Salary increased'
-		ELSE 'Salary decreased'
-	END AS Salary_status
+		WHEN avg_salary_value - avg_salary_previous_year > 0 THEN 'salary increased'
+		ELSE 'salary decreased'
+	END AS salary_status
 FROM cte_avg_salary_previous_year
 WHERE  avg_salary_value - avg_salary_previous_year != 0
+
 ORDER BY year ASC, branch_name ASC;
