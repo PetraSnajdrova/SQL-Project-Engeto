27a40a3a99fcdf04ce431ea1be1ab928f93a9028
PetraSnajdrova <petrasnajdrova@seznam.cz>
2025-08-27 20:10:41 +0200

    Task_3.sql

--- Task_3.sql
+++ Task_3.sql
@@ -5,7 +5,7 @@ WITH cte_avg_goods_value_previous_year AS (
 		DISTINCT year,	
 		avg_goods_value,
 		goods_name,
-		lag(avg_goods_value) OVER (PARTITION BY Goods_name ORDER BY year) AS avg_goods_previous_year
+		lag(avg_goods_value) OVER (PARTITION BY goods_name ORDER BY year) AS avg_goods_previous_year
 	FROM t_petra_snajdrova_project_sql_primary_final tpspspf
 	ORDER BY YEAR ASC, goods_name ASC
 )
@@ -27,4 +27,5 @@ FROM cte_avg_goods_value_percentage
 WHERE goods_status_percentage > 0
 ORDER BY goods_status_percentage ASC, YEAR ASC
 LIMIT 1;
+
 
